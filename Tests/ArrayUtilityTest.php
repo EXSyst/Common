200c8c702fec4085efa789467c35958049154da8
Ener-Getick <egetick@gmail.com>
2015-09-14 15:25:26 -0400

    Applied fixes from StyleCI

--- Tests/ArrayUtilityTest.php
+++ Tests/ArrayUtilityTest.php
@@ -1,16 +1,19 @@
 <?php
+
 namespace EXSyst\Common\Tests;
 
 use EXSyst\Common\ArrayUtility;
 
 class ArrayUtilityTest extends \PHPUnit_Framework_TestCase
 {
-    public function testAssociativeArray() {
+    public function testAssociativeArray()
+    {
         $associativeArray = ['foo' => 'bar', 'foo2' => 'bar2'];
         $this->assertTrue(ArrayUtility::isAssociative($associativeArray), sprintf('Array is associate so ArrayUtility::isAssociative must return true.', $associativeArray));
     }
 
-    public function testSequentialArray() {
+    public function testSequentialArray()
+    {
         $sequentialArray = ['bar', 'bar2', 'foo', 'foo2'];
         $this->assertFalse(ArrayUtility::isAssociative($sequentialArray), sprintf('Array is sequential so ArrayUtility::isAssociative must return false.', $sequentialArray));
     }
